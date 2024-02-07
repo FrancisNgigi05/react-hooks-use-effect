8f8ec63b1b237c305c4cde5a90b9c26f5839a2f8
FrancisNgugi05 <francis.ngugi@student.moringaschool.com>
2024-02-07 04:53:53 +0300

    completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { useState, useEffect } from "react";
+import DogPics from "./DogPics";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+
   useEffect(() => {
-    console.log("useEffect called");
-  });
+    // console.log("useEffect called");
+    document.title = text;
+  }, [text]);
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return (
+    <div>
+      <button onClick={() => setCount((count) => count + 1)}>I've been clicked { count } times</button>
+      <input type="text" placeholder="Type away..." onChange={(e) => setText(e.target.value)}/>
+      <DogPics counter={count}/>
+    </div> );
 }
 
 export default App;
